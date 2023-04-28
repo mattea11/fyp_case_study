94d259915ebc4650a67c0206f54d7f0939b702ce
mattea11 <matteafenech@gmail.com>
2023-04-28 16:38:38 +0200

    rosclient larave server connection wokring, found mast rostopic

--- larva_client.py
+++ larva_client.py
@@ -3,78 +3,98 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from control_msgs.msg import JointControllerState
 import json
-import websocket
+# import websocket
+import socket
 
-# Specify the WebSocket server URL
-websocket_url = "ws://192.168.4.21:8080"
-print('this is the ip ' + websocket_url)
+# # Specify the WebSocket server URL
+# websocket_url = "ws://192.168.4.21:8080"
+# print('this is the ip ' + websocket_url)
 
-# Connect to the WebSocket server
-print('Connecting to server')
-ws = websocket.create_connection(websocket_url)
-print('Connected to Larva server successfully!')
+# # Connect to the WebSocket server
+# print('Connecting to server')
+# ws = websocket.create_connection(websocket_url, debug=True)
+# print('Connected to Larva server successfully!')
 
-ws.send("{\"msg\": \"Client attempting to connect\"}")
-print('sent msg!!!')
+# ws.send("{\"msg\": \"Client attempting to connect\"}")
+# print('sent msg!!!')
+
+
+HOST = "192.168.4.21"
+PORT = 8080
+
+# Create a TCP socket and connect to the server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+# Send a message to the server
+message = 'Hello, server!'
+client_socket.sendall(message.encode())
+
+# Receive a response from the server
+data = client_socket.recv(1024)
+print('Received from server: ', data.decode())
+
+# Close the socket
+client_socket.close()
 
 
 # Callback function for sending JSON objects to the server
-def send_data_to_larva(json_obj):
-    ws.send(json_obj)
-    print('Sent JSON object:', json_obj)
+# def send_data_to_larva(json_obj):
+#     ws.send(json_obj)
+#     print('Sent JSON object:', json_obj)
 
 # Callback function for Odometry data
-def odom_callback(data):
-    lin_vel = data.twist.twist.linear.x
+# def odom_callback(data):
+#     lin_vel = data.twist.twist.linear.x
 
-    # Create JSON object
-    json_obj = json.dumps({"speed": lin_vel})
+#     # Create JSON object
+#     json_obj = json.dumps({"speed": lin_vel})
 
-    # Send JSON object to server
-    send_data_to_larva(json_obj)
+#     # Send JSON object to server
+#     send_data_to_larva(json_obj)
 
-# Callback function for LaserScan data
-def scan_callback(scan_data):
-    ranges = scan_data.ranges
+# # Callback function for LaserScan data
+# def scan_callback(scan_data):
+#     ranges = scan_data.ranges
 
-    # Create JSON object
-    json_obj = json.dumps({"dist": ranges})
+#     # Create JSON object
+#     json_obj = json.dumps({"dist": ranges})
 
-    # Send JSON object to server
-    send_data_to_larva(json_obj)
+#     # Send JSON object to server
+#     send_data_to_larva(json_obj)
 
-# Callback function for JointControllerState data
-def arm_callback(data):
-    arm_angel = data.process_value
+# # Callback function for JointControllerState data
+# def arm_callback(data):
+#     arm_angel = data.process_value
 
-    # Create JSON object
-    json_obj = json.dumps({"mast_angle": arm_angel * 180.0 / 3.14})
+#     # Create JSON object
+#     json_obj = json.dumps({"mast_angle": arm_angel * 180.0 / 3.14})
 
-    # Send JSON object to server
-    send_data_to_larva(json_obj)
+#     # Send JSON object to server
+#     send_data_to_larva(json_obj)
 
-def listener():
-    # Initialize the node
-    rospy.init_node('data_collector', anonymous=True)
+# def listener():
+#     # Initialize the node
+#     rospy.init_node('data_collector', anonymous=True)
 
-    # Subscribe to the topics
-    rospy.Subscriber("/curiosity_mars_rover/odom", Odometry, odom_callback)
-    rospy.Subscriber('/curiosity_mars_rover/camera_fronthazcam/scan', LaserScan, scan_callback)
-    rospy.Subscriber("/curiosity_mars_rover/arm_01_joint_position_controller/state", JointControllerState, arm_callback)
+#     # Subscribe to the topics
+#     rospy.Subscriber("/curiosity_mars_rover/odom", Odometry, odom_callback)
+#     rospy.Subscriber('/curiosity_mars_rover/camera_fronthazcam/scan', LaserScan, scan_callback)
+#     rospy.Subscriber("/curiosity_mars_rover/arm_01_joint_position_controller/state", JointControllerState, arm_callback)
 
-    # Spin until shutdown
-    rospy.spin()
+#     # Spin until shutdown
+#     rospy.spin()
 
-if __name__ == '__main__':
-    try:
-        listener()
-    except rospy.ROSInterruptException:
-        pass
-    finally:
-        # Close the WebSocket connection
-        if ws:
-            print('Closing connection')
-            ws.close()
+# if __name__ == '__main__':
+#     try:
+#         listener()
+#     except rospy.ROSInterruptException:
+#         pass
+#     finally:
+#         # Close the WebSocket connection
+#         if ws:
+#             print('Closing connection')
+#             ws.close()
 
 
 
--- ros_mon.py
+++ ros_mon.py
@@ -1,143 +1,147 @@
-import rospy
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan
-from control_msgs.msg import JointControllerState
-import json
-
-# Global variables to store data
-lin_vel = 0.0
-ranges = []
-arm_angel = 0.0
-
-def odom_callback(data):
-    global lin_vel
-    lin_vel = data.twist.twist.linear.x
-
-    json_obj = {"speed": lin_vel}
-
-    speed_obj = json.dumps(json_obj)
-    print('speed object: ' + speed_obj)
-    return speed_obj
-
-def scan_callback(scan_data):
-    global ranges
-    ranges = scan_data.ranges
-
-    # change depending on what i plan on using
-    json_obj = {"dist": ranges}
-
-    dist_obj = json.dumps(json_obj)
-    print('dist object: ' + dist_obj)
-    return dist_obj
-
-def arm_callback(data):
-    global arm_angel
-    arm_angel = data.process_value
-    
-    json_obj = {"mast_angle": arm_angel * 180.0 / 3.14}
-
-    mast_obj = json.dumps(json_obj)
-    print('mast object: ' + mast_obj)
-    return mast_obj
-
-def listener():
-    # Initialize the node
-    rospy.init_node('data_collector', anonymous=True)
-
-    # Subscribe to the topics
-    # rospy.Subscriber("/curiosity_mars_rover/odom", Odometry, odom_callback)
-    # rospy.Subscriber('/curiosity_mars_rover/camera_fronthazcam/scan', LaserScan, scan_callback)
-    # rospy.Subscriber("/curiosity_mars_rover/mast_02_joint_position_controller/state", JointControllerState, arm_callback) #horizontal turn ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # rospy.Subscriber("/curiosity_mars_rover/mast_cameras_joint_position_controller/state", JointControllerState, arm_callback) vertical ~~~~~~~~~~~~~~~~~~~~~~
-
-    
-    # Spin until shutdown
-    rospy.spin()
-
-if __name__ == '__main__':
-    listener()
-
-
-
-
-
 # import rospy
-# import numpy as np
-# from sensor_msgs.msg import Image, CameraInfo
-# from cv_bridge import CvBridge
-# import cv2
+# from nav_msgs.msg import Odometry
+# from sensor_msgs.msg import LaserScan
+# from control_msgs.msg import JointControllerState
+# import json
 
-# class NavcamDistanceEstimator:
-#     def __init__(self):
-#         rospy.init_node('navcam_distance_estimator')
-#         self.bridge = CvBridge()
-#         self.left_image = None
-#         self.right_image = None
-#         self.left_camera_info = None
-#         self.right_camera_info = None
-#         self.focal_length = None
-#         self.baseline = None
+# # Global variables to store data
+# lin_vel = 0.0
+# ranges = []
+# arm_angel = 0.0
 
-#         # Subscribe to the image_raw topics of the left and right cameras
-#         self.sub_left = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/left/image_raw', Image, self.left_callback)
-#         self.sub_right = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/right/image_raw', Image, self.right_callback)
+# def odom_callback(data):
+#     global lin_vel
+#     lin_vel = data.twist.twist.linear.x
 
-#         # Subscribe to the camera_info topics of the left and right cameras
-#         self.sub_left_info = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/left/camera_info', CameraInfo, self.left_info_callback)
-#         self.sub_right_info = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/left/camera_info', CameraInfo, self.right_info_callback)
+#     json_obj = {"speed": lin_vel}
 
-#     def left_callback(self, msg):
-#         self.left_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+#     speed_obj = json.dumps(json_obj)
+#     print('speed object: ' + speed_obj)
+#     return speed_obj
 
-#     def right_callback(self, msg):
-#         self.right_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+# def scan_callback(scan_data):
+#     global ranges
+#     ranges = scan_data.ranges
 
-#     def left_info_callback(self, msg):
-#         self.left_camera_info = msg
-#         if self.right_camera_info is not None:
-#             self.calculate_baseline_and_focal_length()
+#     # change depending on what i plan on using
+#     json_obj = {"dist": ranges}
 
-#     def right_info_callback(self, msg):
-#         self.right_camera_info = msg
-#         if self.left_camera_info is not None:
-#             self.calculate_baseline_and_focal_length()
+#     dist_obj = json.dumps(json_obj)
+#     print('dist object: ' + dist_obj)
+#     return dist_obj
 
-#     def calculate_baseline_and_focal_length(self):
-#         left_fx = self.left_camera_info.K[0]
-#         right_fx = self.right_camera_info.K[0]
-#         baseline = abs(self.left_camera_info.P[3] / left_fx - self.right_camera_info.P[3] / right_fx)
-#         self.baseline = baseline
-#         self.focal_length = (left_fx + right_fx) / 2
+# def arm_callback(data):
+#     global arm_angel
+#     arm_angel = data.process_value
+    
+#     json_obj = {"mast_angle": arm_angel * 180.0 / 3.14}
 
-#     def calculate_distance(self):
-#         if self.left_image is None or self.right_image is None or self.focal_length is None or self.baseline is None:
-#             return None
+#     mast_obj = json.dumps(json_obj)
+#     print('mast object: ' + mast_obj)
+#     return mast_obj
 
-#         # Convert images to grayscale
-#         left_gray = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2GRAY)
-#         right_gray = cv2.cvtColor(self.right_image, cv2.COLOR_BGR2GRAY)
+# def listener():
+#     # Initialize the node
+#     rospy.init_node('data_collector', anonymous=True)
 
-#         # Perform block matching algorithm
-#         stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-#         disparity = stereo.compute(left_gray, right_gray)
+#     # Subscribe to the topics
+#     # rospy.Subscriber("/curiosity_mars_rover/odom", Odometry, odom_callback)
+#     # rospy.Subscriber('/curiosity_mars_rover/camera_fronthazcam/scan', LaserScan, scan_callback)
+#     # rospy.Subscriber("/curiosity_mars_rover/mast_02_joint_position_controller/state", JointControllerState, arm_callback) #horizontal turn ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     # rospy.Subscriber("/curiosity_mars_rover/mast_cameras_joint_position_controller/state", JointControllerState, arm_callback) vertical ~~~~~~~~~~~~~~~~~~~~~~
 
-#         # Calculate depth from disparity
-#         depth = self.focal_length * self.baseline / disparity
-
-#         # Find minimum non-zero depth value
-#         min_depth = np.min(depth[depth > 0])
-
-#         # Calculate distance from minimum depth value
-#         distance = min_depth
-
-#         return distance
+    
+#     # Spin until shutdown
+#     rospy.spin()
 
 # if __name__ == '__main__':
-#     estimator = NavcamDistanceEstimator()
-#     rate = rospy.Rate(10) # 10 Hz
+#     listener()
 
-#     while not rospy.is_shutdown():
-#         distance = estimator.calculate_distance()
-#         if distance is not None:
-#             rospy.loginfo('Distance: %f', distance)
-#         rate.sleep()
+
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CameraInfo
+
+class StereoDepth:
+    def __init__(self):
+        rospy.init_node('stereo_depth', anonymous=True)
+        
+        self.bridge = CvBridge()
+        
+        # Subscribe to the left and right camera topics
+        self.left_image_sub = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/left/image_raw', Image, self.left_image_callback)
+        self.left_info_sub = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/left/camera_info', CameraInfo, self.left_info_callback)
+        self.right_image_sub = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/right/image_raw', Image, self.right_image_callback)
+        self.right_info_sub = rospy.Subscriber('/curiosity_mars_rover/camera_navcam/right/camera_info', CameraInfo, self.right_info_callback)
+        
+        self.fx, self.fy, self.cx, self.cy = None, None, None, None 
+        self.left_image, self.right_image = None, None
+        
+        self.depth_scale = 0.001  # Conversion factor for disparity to depth (1 pixel = 1mm at 1 meter)
+        
+    def left_image_callback(self, msg):
+        try:
+            self.left_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        except CvBridgeError as e:
+            rospy.logerr(e)
+    
+    def left_info_callback(self, msg):
+        self.fx = msg.K[0]
+        self.fy = msg.K[4]
+        self.cx = msg.K[2]
+        self.cy = msg.K[5]
+        print(msg.K[0] ,msg.K[4] ,msg.K[2] ,msg.K[5])
+        
+    def right_image_callback(self, msg):
+        try:
+            print('pls?')
+            self.right_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        except CvBridgeError as e:
+            rospy.logerr(e)
+    
+    def right_info_callback(self, msg):
+        pass
+    
+    def calculate_depth(self):
+        if self.left_image is not None and self.right_image is not None:
+            # Convert images to grayscale
+            left_gray = cv2.cvtColor(self.left_image, cv2.COLOR_BGR2GRAY)
+            right_gray = cv2.cvtColor(self.right_image, cv2.COLOR_BGR2GRAY)
+            
+            # Compute disparity map
+            stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+            disparity = stereo.compute(left_gray, right_gray)
+            
+            # Compute depth map
+            depth = self.fx * self.fy / (disparity + 0.0001) * self.depth_scale
+            
+            # Calculate distance to object at center of image
+            x = int(self.cx)
+            y = int(self.cy)
+            distance = depth[y, x]
+            
+            rospy.loginfo('Distance to object: {:.2f} meters'.format(distance))
+            
+    def run(self):
+        print('bef rate')
+        rate = rospy.Rate(10)  # Hz
+        print('after rate')
+        while not rospy.is_shutdown():
+            print('bef calc depth')
+            self.calculate_depth()
+            print('after calc depth')
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        print('lol')
+        sd = StereoDepth()
+        print('lol2')
+        sd.run()
+        print('lol3')
+    except rospy.ROSInterruptException:
+        pass

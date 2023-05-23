358deb0cc95210e8d15a182edfb7f4c5805658b2
mattea11 <matteafenech@gmail.com>
2023-05-23 10:28:00 +0200

    FINAL YEAY

--- rosMon_larvaClient/create_json.py
+++ rosMon_larvaClient/create_json.py
@@ -10,8 +10,6 @@ class create_json:
                 "turn_z": z,
                 "turn_w": w
             }
-            # ,
-            # "change_speed": speed
         }
         return json.dumps(data)
 
@@ -24,29 +22,11 @@ class create_json:
         my_dict = {'change_vert_ang': angle}
         json_object = json.dumps(my_dict)
         return json_object
-        
-    # def curr_dist(distances):
-    #     my_dict = {'curr_distances': distances}
-    #     json_object = json.dumps(my_dict)
-    #     return json_object
-
-    # def curr_speed(speed):
-    #     my_dict = {'curr_speed': speed}
-    #     json_object = json.dumps(my_dict)
-    #     return json_object
-
-    # def curr_mast_ang(angle):
-    #     my_dict = {'curr_vert_ang': angle}
-    #     json_object = json.dumps(my_dict)
-    #     return json_object
     
     def merge_json(json1, json2):
-        # Convert the JSON strings to Python dictionaries
         dict1 = json.loads(json1)
         dict2 = json.loads(json2)
-        # Merge the dictionaries
         merged_dict = {**dict1, **dict2}
-        # Convert the merged dictionary back to a JSON string
         merged_json = json.dumps(merged_dict) + '\n'
         return merged_json
     
@@ -55,6 +35,5 @@ class create_json:
         json_object = json.dumps(my_dict)
         return json_object
 
-    # Callback function for sending JSON objects to the server
     def send_data_to_larva(client_socket, json_obj):
         client_socket.sendall(json_obj.encode())
--- rosMon_larvaClient/ros_mon.py
+++ rosMon_larvaClient/ros_mon.py
@@ -11,47 +11,36 @@ class ros_mon:
         rospy.init_node('rover_mon_node', anonymous=True)
 
 
-    #each of tehf ollowing functions collects data for their respective property at teh tiem and create a json object of the information
+    #each of the following functions collects data for their respective property at the time and create a json object of the information
     def get_latest_distance(self):
-        # if not rospy.core.is_initialized(): 
-        # rospy.init_node('rover_distance_node', anonymous=True)
         data = rospy.wait_for_message('/merged_scan', LaserScan)
-        print(1)
 
         dist = data.ranges
         smallest = min(dist)
-        smallest = f"{smallest:.3f}"
-        print(2)
+        smallest = f"{smallest:.2f}"
         my_dict = {'curr_distances': smallest}
         json_speed = json.dumps(my_dict)
 
-        # rospy.signal_shutdown("Finished getting latest distance.")
         return json_speed
 
     def get_latest_speed(self):
-        # if not rospy.core.is_initialized():
-        # rospy.init_node('rover_speed_node', anonymous=True)
         data = rospy.wait_for_message('/curiosity_mars_rover/odom', Odometry)
 
         speed = data.twist.twist.linear.x
-        speed = f"{speed:.6f}"
+        speed = f"{speed:.2f}"
 
         my_dict = {'curr_speed': speed}
         json_speed = json.dumps(my_dict)
 
-        # rospy.signal_shutdown("Finished getting latest speed.")
         return json_speed
 
-    def get_latest_arm_vert(self): 
-        # if not rospy.core.is_initialized():
-        # rospy.init_node('vert_angle_node', anonymous=True)          
+    def get_latest_arm_vert(self):        
         data = rospy.wait_for_message('/curiosity_mars_rover/mast_cameras_joint_position_controller/state', JointControllerState)
 
         angle = data.process_value
-        angle = angle
+        angle = f"{angle:.2f}"
 
         my_dict = {'curr_vert_ang': angle}
         json_angle = json.dumps(my_dict)
 
-        # rospy.signal_shutdown("Finished getting latest angle.")
         return json_angle
